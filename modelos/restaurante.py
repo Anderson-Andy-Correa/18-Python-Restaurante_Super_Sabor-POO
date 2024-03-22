ffe634d94b38e4b97397ead2a223f9328ee26924
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-21 21:25:15 -0300

    Alterando a propiedade do atributo ativo

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,9 +1,10 @@
 class Restaurante:
     restaurantes = []
+
     def __init__(self, nome, categoria):
         self.nome = nome
         self.categoria = categoria
-        self.ativo = False
+        self._ativo = False
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -11,8 +12,15 @@ class Restaurante:
 
 
     def listar_restaurantes():
+        titulo_listar = f'{"Nome do restaurante".ljust(25)} │ {"Categoria".ljust(25)} │ {"Status"}'
+        print(titulo_listar)
+        print('─'*len(titulo_listar))
         for restaurante in Restaurante.restaurantes:
-            print(f'{restaurante.nome} | {restaurante.categoria} | {restaurante.ativo}')
+            print(f'{restaurante.nome.ljust(25)} │ {restaurante.categoria.ljust(25)} │ {restaurante.ativo}')
+
+    @property
+    def ativo(self):
+        return '✅' if self._ativo else '❎'
 
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
