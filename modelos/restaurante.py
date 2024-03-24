80d5ed336487229a3700a3fc1350ad19893c2c3c
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-24 13:50:59 -0300

    Criando metodo de media_avaliacao

--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -32,3 +32,11 @@ class Restaurante:
 
     def receber_acaliacao(self, cliente, nota):
         self._avaliacao.append(Avaliacao(cliente, nota))
+
+    @property
+    def media_avaliacao(self):
+        if not self._avaliacao:
+            return 0
+        soma_notas = sum(avaliacao._nota for avaliacao in self._avaliacao)
+        return round(soma_notas / len(self._avaliacao), 1)
+    
