dbd238678f3efe184e75687ed88a54206397e980
Anderson Corrêa <106445568+Anderson-Andy-Correa@users.noreply.github.com>
2024-03-24 13:42:28 -0300

    Criando a avaliacao

--- app.py
+++ app.py
@@ -1,6 +1,9 @@
 from modelos.restaurante import Restaurante
 
 restaurante_praca = Restaurante('Praça', 'Gourmet')
+restaurante_praca.receber_acaliacao('Andy', 10)
+restaurante_praca.receber_acaliacao('Igor', 8)
+restaurante_praca.receber_acaliacao('Barbara', 5)
 restaurante_praca.alternar_estado()
 restaurante_mexicano = Restaurante('Mexican Food', 'Mexicano')
 restaurante_japones = Restaurante('Japa', 'Japonesa')
--- modelos/restaurante.py
+++ modelos/restaurante.py
@@ -1,3 +1,5 @@
+from modelos.avaliacao import Avaliacao
+
 class Restaurante:
     restaurantes = []
 
@@ -5,6 +7,7 @@ class Restaurante:
         self.nome = nome
         self.categoria = categoria
         self._ativo = False
+        self._avaliacao = []
         Restaurante.restaurantes.append(self)
 
     def __str__(self):
@@ -25,3 +28,7 @@ class Restaurante:
 
     def alternar_estado(self):
         self._ativo = not self._ativo
+
+
+    def receber_acaliacao(self, cliente, nota):
+        self._avaliacao.append(Avaliacao(cliente, nota))
